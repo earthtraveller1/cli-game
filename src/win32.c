dd5f61a2f644cbefda1232c0f76ede4d5c42a392
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-11-26 16:53:39 -0500

    Implemented the system specific functions.

---
+++ src/win32.c
@@ -0,0 +1,48 @@
+#include <Windows.h>
+#include <process.h>
+#include <conio.h>
+
+#include "platform.h"
+
+static HANDLE global_mutex;
+
+static HANDLE getch_thread;
+
+int get_single_char()
+{
+    return _getch();
+}
+
+void get_terminal_size(uint32_t* width, uint32_t* height)
+{
+    CONSOLE_SCREEN_BUFFER_INFO console_info;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &console_info);
+    
+    *width = console_info.srWindow.Right - console_info.srWindow.Left + 1;
+    *height = console_info.srWindow.Bottom - console_info.srWindow.Top + 1;
+}
+
+void create_global_mutex()
+{
+    global_mutex = CreateMutexW(NULL, FALSE, NULL);
+}
+
+void wait_for_mutex()
+{
+    WaitForSingleObject(global_mutex, INFINITE);
+}
+
+void release_mutex()
+{
+    ReleaseMutex(global_mutex);
+}
+
+void start_getch_loop(getch_loop_proc proc)
+{
+    getch_thread = (HANDLE)_beginthread(proc, 64, NULL);
+}
+
+void join_getch_loop()
+{
+    WaitForSingleObject(getch_thread, INFINITE);
+}
